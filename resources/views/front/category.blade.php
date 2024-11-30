40bdec0663b9fdeacf8caa4e214ebe641a2f3714
RifkySetyaRahman <Rifkyalahmed@gmail.com>
2024-11-30 17:15:16 +0800

    tambahan coding

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -1,9 +1,11 @@
+
 @extends('layouts.app')
 @section('title', 'Training Center Poliban')
 @section('content')
 
+
 <div class="h-[112px]">
-        <x-nav/>
+    <x-nav/>
     </div>
     <section id="Category" class="w-full max-w-[1280px] mx-auto px-[52px] mt-[52px] mb-[100px]">
         <div class="flex flex-col gap-9">
@@ -19,27 +21,80 @@
             </div>
             <div class="grid grid-cols-3 gap-6">
 
-            @foreach ($category->workshops as $workshop)
-<a href="{{ route('front.details', $workshop->slug) }}" class="card">
-    <div class="flex flex-col h-full justify-between rounded-3xl p-6 gap-9 bg-white">
-        <div class="flex flex-col gap-[18px]">
-            <div class="flex items-center gap-3">
-                <img src="{{ storage_url($workshop->instructor->avatar) }}" class="w-16 h-16 rounded-full flex shrink-0 overflow-hidden" 
-                    alt="Instructor Avatar">
-            </div>
-            <div class="flex flex-col gap-[2px]">
-                <p class="font-semibold text-lg leading-[27px]">
-                    {{ $itemNewWorkshop->instructor->name }}
-                </p>
-                <p class="font-medium text-aktiv-grey">{{ $workshop->instructor->occupation }}
-                </p>
-            </div>
-        </div>
+            @forelse ($category->workshops as $itemNewWorkshop)
+            <a href="{{route('front.details', $itemNewWorkshop->slug)}}" class="card">
+                    <div class="flex flex-col h-full justify-between rounded-3xl p-6 gap-9 bg-white">
+                        <div class="flex flex-col gap-[18px]">
+                            <div class="flex items-center gap-3">
+                                <div class="w-16 h-16 rounded-full flex shrink-0 overflow-hidden">
+                                    <img src="{{Storage::url($itemNewWorkshop->instructor->avatar)}}" class="w-full h-full object-cover" alt="avatar">
+                                </div>
+                                <div class="flex flex-col gap-[2px]">
+                                    <p class="font-semibold text-lg leading-[27px]">{{$itemNewWorkshop->instructor->name}}</p>
+                                    <p class="font-medium text-aktiv-grey">{{$itemNewWorkshop->instructor->occupation}}</p>
+                                </div>
+                            </div>
 
-        <div class="thumbnail-container relative h-[200px] rounded-xl bg-[#D9D9D9] overflow-hidden">
-            <img src="{{ storage_url($workshop->thumbnail) }}" class="w-full h-full object-cover" alt="Workshop Thumbnail">
-            @if($workshop->is_open)
-                @if($workshop->has_started)
+                            <div class="thumbnail-container relative h-[200px] rounded-xl bg-[#D9D9D9] overflow-hidden">
+                                <img src="{{asset('assets/images/thumbnails/thumbnail1.png')}}" class="w-full h-full object-cover" alt="thumbnail">
+                                @if($itemNewWorkshop->is_open)
+                                    @if($itemNewWorkshop->has_started)
+                                <div class="absolute top-3 left-3 flex items-center rounded-full py-3 px-5 gap-1 bg-aktiv-orange text-white z-10">
+                                <img src="{{asset('assets/images/icons/timer-start.svg')}}" class="w-6 h-6" alt="icon">
+                                <span class="font-semibold">STARTED</span>
+                                </div>
+                            </div>
+                            @else
+                            <div class="absolute top-3 left-3 flex items-center rounded-full py-3 px-5 gap-1
+                            bg-aktiv-green text-white z-10">
+                                <img src="{{asset('assets/images/icons/medal-star.svg' )}}" class="w-6 h-6" alt="icon">
+                                <span class="font-semibold">OPEN</span>
+                            </div>
+                            @endif
+                        @else
+                            <div class="absolute top-3 left-3 flex items-center rounded-full py-3 px-5 gap-1
+                            bg-aktiv-red text-white z-10">
+                                <img src="{{asset('assets/images/icons/sand-timer.svg' )}}" class="w-6 h-6" 
+                                alt="icon">
+                            <span class="font-semibold">CLOSED</span>
+                            </div>
+                        @endif
+                        </div>    
+                            <div class="card-detail flex flex-col gap-2">
+                                <div class="flex items-center gap-3">
+                                    <div class="flex items-center gap-1">
+                                        <img src="{{asset('assets/images/icons/calendar-2.svg')}}" class="w-6 h-6 flex shrink-0" alt="icon">
+                                        <span class="font-medium text-aktiv-grey">{{$itemNewWorkshop->started_at->format('M d, Y')}}</span>
+                                    </div>
+                                    <div class="flex items-center gap-1">
+                                        <img src="{{asset('assets/images/icons/timer.svg')}}" class="w-6 h-6 flex shrink-0" alt="icon">
+                                        <span class="font-medium text-aktiv-grey">{{$itemNewWorkshop->time_at->format('h:i A')}}</span>
+                                    </div>
+                                </div>
+                                <h3 class="title min-h-[56px] font-semibold text-xl line-clamp-2 hover:line-clamp-none">
+                                    {{$itemNewWorkshop->name}}
+                                </h3>
+                                <p class="font-medium text-aktiv-grey">
+                                    {{$itemNewWorkshop->category->name}}
+                                </p>
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center gap-[6px]">
+                                <p class="font-semibold text-2xl leading-8 text-aktiv-red">
+                                    Rp {{number_format($itemNewWorkshop->price, 0, ',', '.' )}}
+                                </p>
+                                <p class="font-medium text-aktiv-grey">/person</p>
+                            </div>
+                            <img src="{{asset('assets/images/icons/arrow-right.svg')}}" class="w-12 h-12 flex shrink-0" alt="icon">
+                        </div>
+                    </div>
+                </a>
+
+            @empty
+                <p>belum ada data workshop terbaru</p>
+            @endforelse
+
             </div>
         </div>
     </section>
@@ -78,4 +133,4 @@
         </div>
     </footer>
 
-@endsection
+    @endsection
